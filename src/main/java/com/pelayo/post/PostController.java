6a41d5537a062e34ca88fb0b4514b6eda37edc76
kristapelayo <pelayokrista29@gmail.com>
2025-05-16 15:54:01 +0800

    Updated

--- src/main/java/com/pelayo/post/PostController.java
+++ src/main/java/com/pelayo/post/PostController.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/pelayo/posts")
 public class PostController {
 
     @Autowired
